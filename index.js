7862a226de3551457010d4b8f1ba4d5bf3b85b83
masum362 <masumahmed64077@gmail.com>
2024-08-22 22:22:58 +0600

    loops added

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const arr = [ 1,2,3,4,5,6,7,8,9,10];
 // arr.length //return the length of the array
 // arr[1] //return the index element of the array
 // arr.sort() //sort the arra
+
+
+// for (let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// let i = 0;
+
+// while (i<arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
