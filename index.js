514e385b2f1c0ef949cbbbe710fd983986df3da2
masum362 <masumahmed64077@gmail.com>
2024-08-22 22:17:51 +0600

    added the index.js page

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// array 
+const arr = [ 1,2,3,4,5,6,7,8,9,10];
+
+// array functions
+// arr.length //return the length of the array
+// arr[1] //return the index element of the array
+// arr.sort() //sort the arra
