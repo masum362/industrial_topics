63d5a57f1d6b039e9df0ecb673e3b03170f5f3f2
masum362 <masumahmed64077@gmail.com>
2024-08-29 19:20:53 +0600

    day 19 updated

--- day 19/script.js
+++ day 19/script.js
@@ -3,12 +3,11 @@
 const headingTag = document.getElementsByTagName('h1')
 const changeBtn = document.getElementById("changeBtn");
 const headingByClass = document.getElementsByClassName('heading')
+const something = document.getElementsByName('something') //get the elements with their name
+const anotherOne = document.querySelector('.other') //select an item with their unique value..like id (#other),class(.other),tag(h1) it select the first element with the selector
+const finalOne = document.querySelectorAll('.other') //select all elements with the selector
 
 const headingArr = [...headingByClass]
-
-
-
-
 
 const handleBtnClick = () => {
     console.log('clicked')
