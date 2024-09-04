3b7c803f7d7cb9e486d1fc683f11cdab1e35d459
masum362 <masumahmed64077@gmail.com>
2024-09-04 13:15:03 +0600

    added day 22 functions

--- day 22/script.js
+++ day 22/script.js
@@ -1,19 +1,43 @@
 const allPostContainer = document.getElementById("allPostContainer");
 const readPostContainer = document.getElementById("readPostContainer");
 
-const fetchFunc = async (url) => {
-  try {
-    const response = await fetch(url);
-    const posts = await response.json();
-    return posts;
-  } catch (error) {
-    return error;
-  }
-};
+
+// return the posts from uri
+const fetchFunc = async (url) => {};
+
+
+// fetch all posts
+const fetchPosts =async (api) => {}
+
+// show post in domain 
+const showPosts = (post) => {};
+
+
+// handle the user post click and add it to read container
+const handlePostClick = (postId) => {}
+
+
+// get the latest posts
+const getLatestPost = async () => {}
+
+
+// search a post by it's category
+const handleSearch = () => {}
+
+
+// get all posts on loading
+fetchPosts();
+// get latest posts on loading
+getLatestPost();
 
 
 
-const fetchPosts =async (api) => {
 
-}
+// get all posts api 
+// const url = "https://openapi.programming-hero.com/api/retro-forum/posts";
 
+// get latest posts api 
+// const url = "https://openapi.programming-hero.com/api/retro-forum/latest-posts";
+
+// search by category
+// const api = `https://openapi.programming-hero.com/api/retro-forum/posts?category=${query}`
